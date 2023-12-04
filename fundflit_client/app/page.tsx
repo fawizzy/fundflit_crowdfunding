e2c3a7ddefb136151c8939ec0d906aa6efed19fa
Chris Awoke <uchechristian27@gmail.com>
2023-12-04 01:05:25 +0100

    new updates

--- fundflit_client/app/page.tsx
+++ fundflit_client/app/page.tsx
@@ -1,5 +1,10 @@
+import { Hero } from "@/components";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main>Home</main>
-  )
+    <main className="overflow-hidden">
+      <Hero />
+    </main>
+  );
 }
